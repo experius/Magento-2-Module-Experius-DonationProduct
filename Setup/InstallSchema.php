77cf754cb03797f1620ab960cef0d48610e3c193
Jeroen Coppelmans <jeroen@experius.nl>
2017-11-14 17:59:40 +0000

    [TASK] added extra column in DB 'created_at'

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,15 +1,14 @@
 <?php
 
-
 namespace Experius\DonationProduct\Setup;
 
+use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
 class InstallSchema implements InstallSchemaInterface
 {
-
     /**
      * {@inheritdoc}
      */
@@ -22,75 +21,74 @@ class InstallSchema implements InstallSchemaInterface
 
         $table_experius_donationproduct_donations = $setup->getConnection()->newTable($setup->getTable('experius_donations'));
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'donations_id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            Table::TYPE_INTEGER,
             null,
-            array('identity' => true,'nullable' => false,'primary' => true,'unsigned' => true,),
+            [
+                'identity' => true,
+                'nullable' => false,
+                'primary' => true,
+                'unsigned' => true,
+            ],
             'Entity ID'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'name',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            Table::TYPE_TEXT,
             255,
             [],
             'name'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'sku',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            Table::TYPE_TEXT,
             255,
             [],
             'sku'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'amount',
-            \Magento\Framework\DB\Ddl\Table::TYPE_DECIMAL,
+            Table::TYPE_DECIMAL,
             '12,4',
             [],
             'amount'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'order_id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            Table::TYPE_INTEGER,
             null,
             [],
             'order_id'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'order_status',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            Table::TYPE_TEXT,
             null,
             [],
             'order_status'
         );
-        
 
-        
         $table_experius_donationproduct_donations->addColumn(
             'invoiced',
-            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            Table::TYPE_BOOLEAN,
             null,
             [],
             'invoiced'
         );
-        
+
+        $table_experius_donationproduct_donations->addColumn(
+            'created_at',
+            Table::TYPE_TIMESTAMP,
+            null,
+            [],
+            'Creation date'
+        );
 
         $setup->getConnection()->createTable($table_experius_donationproduct_donations);
 
