d2854efba11b1dd4781dff03df9ab72b6f83bc18
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-11-26 20:14:10 +0100

    [TASK] added setNoDiscount on quote item

---
+++ Observer/Sales/QuoteItemSaveBefore.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * A Magento 2 module named Experius/DonationProduct
+ * Copyright (C) 2017 Derrick Heesbeen
+ *
+ * This file is part of Experius/DonationProduct.
+ *
+ * Experius/DonationProduct is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Experius\DonationProduct\Observer\Sales;
+
+use Experius\DonationProduct\Model\Product\Type\Donation;
+
+class QuoteItemSaveBefore implements \Magento\Framework\Event\ObserverInterface
+{
+
+    /**
+     * Execute observer
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ) {
+        /* @var $item \Magento\Quote\Model\Quote\Item */
+
+        $item = $observer->getItem();
+
+        if ($item->getProduct()->getTypeId()==Donation::TYPE_CODE) {
+            $item->setNoDiscount(1);
+        }
+
+        return $this;
+    }
+}
